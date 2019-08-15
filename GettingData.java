98baa195da90115313084823d46480d873495c14
newman001 <45822324+newman001@users.noreply.github.com>
2019-08-15 16:36:55 +0500

    Add files via upload
    getter and setter

---
+++ GettingData.java
@@ -0,0 +1,22 @@
+package com.studyjava.lesson15;
+
+public class GettingData {
+    private int data;
+    private boolean flag;
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+}
