6d47ffc5924a153f43abd8aa1e8f4c1317e3efd2
newman001 <45822324+newman001@users.noreply.github.com>
2019-08-15 16:25:36 +0500

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,20 @@
+package com.studyjava.lesson15;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args)
+    {
+        int[][] multiplyTab = new int[10][10];
+
+        for (int i=0; i < 10; i++)
+        {
+            for(int j=0; j < 10; j++)
+            {
+                multiplyTab[i][j] = (i + 1) * (j + 1);
+                // вывод ряда чисел, разделенных знаком табуляции
+                System.out.print(multiplyTab[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
